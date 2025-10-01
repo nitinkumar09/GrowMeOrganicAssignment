96562e981e0ca12ddc786c08f9f56be83a738f99
nitinkumar09 <kumarnitin83086@gmail.com>
2025-10-02 01:09:51 +0530

    Some changes

--- src/services/artworkService.ts
+++ src/services/artworkService.ts
@@ -2,13 +2,11 @@ import { ArtworkAPIResponse } from '../types/artwork';
 
 const API_BASE_URL = 'https://api.artic.edu/api/v1/artworks';
 const ROWS_PER_PAGE = 12;
-
 export const fetchArtworks = async (page: number): Promise<ArtworkAPIResponse> => {
   try {
     const response = await fetch(
       `${API_BASE_URL}?page=${page}&limit=${ROWS_PER_PAGE}&fields=id,title,place_of_origin,artist_display,inscriptions,date_start,date_end`
     );
-
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
